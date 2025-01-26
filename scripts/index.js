637f9eb985de39db701f72eb2cf0d33cd41d0681
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2025-01-26 20:40:21 +0300

    feat(js): add function to extract actions from key elements

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,14 @@ const displayValue = document.querySelector('#display-value-text');
 
 // Retrieve all key elements
 const keyButtons = document.querySelectorAll('.key');
+
+// Extract actions from key elements
+function getKeyActions() {
+    // Iterate through each key button
+    keyButtons.forEach((key) => {
+        // Get key action
+        const keyAction = key.firstElementChild.textContent;
+        // Log action
+        console.log(keyAction)
+    });
+}
