6cc9af07b8433d52ddf8cacaa45bec00d3312611
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2025-03-23 21:23:19 +0300

    refactor(js): improve validation for incomplete expressions
    - Flags cases where an opening parenthesis is followed only
      by a number with no operator.
    - Ensures valid cases like nested expressions and functions
      are allowed.
    - Simplifies regex for better readability and efficiency.

--- scripts/index.js
+++ scripts/index.js
@@ -409,7 +409,7 @@ function validateExpression(expression) {
     }
 
     // Check for other calculation errors
-    else if (/[+−÷×^]$|\((?!.+)|(?:(?<![+−÷×^E\d\.\-]|(?:log|ln|√)\()\d+)$/gi.test(sanitizedExpression)){
+    else if (/[+−÷×^]$|^\(*\d+[^E^()+−÷×]*$|\((?!.+)|(?:(?<![^)])\d+)$/gi.test(sanitizedExpression)){
         // Generic error message for other mathematical errors
         errorMessage = "Error: Well... This is Awkward";
     }
